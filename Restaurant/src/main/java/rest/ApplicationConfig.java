4a75465ac2dbe570728dcefbabd929dd4def9a16
metterytt <metterytt@live.dk>
2018-11-20 09:03:26 +0100

    renaming

--- Restaurant/src/main/java/rest/ApplicationConfig.java
+++ Restaurant/src/main/java/rest/ApplicationConfig.java
@@ -24,7 +24,7 @@ public class ApplicationConfig extends Application {
     resources.add(cors.CorsRequestFilter.class);
     resources.add(cors.CorsResponseFilter.class);
     resources.add(exceptions.GenericExceptionMapper.class);
-        resources.add(rest.DemoResource.class);
+        resources.add(rest.RestaurantAPI.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RegisterEndpoint.class);

2a303be50822d880943ed2634cac373b17a425c2
Oliver <oliverloye@me.com>
2018-12-05 11:14:00 +0100

    Settings.js is tracking

--- restaurant-app/src/Settings.js
+++ restaurant-app/src/Settings.js
@@ -1,4 +1,5 @@
 //const URL = "https://oloye.dk";
 const URL = "http://localhost:8084/Restaurant";
 //const URL = "http://localhost:8080/Restaurant"
+//HEJ MED DIG DETTE ER EN TEST HEJ MED DIG
 export default URL;

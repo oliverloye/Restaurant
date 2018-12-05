2138821b1d74860c6c51eae91bf2f9d9e90664f8
Oliver <oliverloye@me.com>
2018-12-05 11:11:02 +0100

    removed Settings.js from untracking

--- restaurant-app/src/Settings.js
+++ restaurant-app/src/Settings.js
@@ -1,8 +1,4 @@
-const URL = "http://localhost:8084/Restaurant";
 //const URL = "https://oloye.dk";
-<<<<<<< HEAD
-const URL = "http://localhost:8084/Restaurant"
-=======
+const URL = "http://localhost:8084/Restaurant";
 //const URL = "http://localhost:8080/Restaurant"
->>>>>>> 7af13d44e0a7471aee9ca162bc34d6e0f81eb034
 export default URL;
